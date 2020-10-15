836caa395b270e8f014d26b8b3025fc2f6183430
0x66CCFF <389041637@qq.com>
2020-10-16 00:25:55 +0800

    assignment1

--- Practice.java
+++ Practice.java
@@ -1,7 +1,6 @@
-public class Practice
-{
-    public static void main(String[] args)
-    {
-        System.out.print("****************************************\n**  Practice makes perfect\n****************************************\n");
-    }    
+public class Practice {
+    public static void main(String[] args) {
+        System.out.print(
+                "****************************************\n**  Practice makes perfect\n****************************************\n");
+    }
 }
