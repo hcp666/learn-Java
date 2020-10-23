21144e1464bf96be986bad32bf288408c4271051
0x66CCFF <389041637@qq.com>
2020-10-23 18:42:38 +0800

    moved files to package path

---
+++ net/0x66ccff/java/assignments/2/Prime.java
@@ -0,0 +1,19 @@
+public class Prime {
+    public static void main(String args[]) {
+        int max = 100;
+        int array[] = new int[max + 1];
+        for (int i = 3; i <= max / 2; i += 2) {
+            if (array[i] == 1) {
+                continue;
+            }
+            for (int j = 2; i * j <= max; j++) {
+                array[i * j] = 1;
+            }
+        }
+        for (int i = 51; i <= max; i += 2) {
+            if (array[i] == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}
